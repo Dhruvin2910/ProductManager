a8e46fd21c4c3da867fb7b93a24f1cbddfc9811e
Dhruvin2910 <dhruvinpatel321@gmail.com>
2024-12-18 00:22:21 +0530

    added GET /products route to retrieve products

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+//initialized app and middleware
+const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/test", (req, res) => {
-    res.send("Test successfull...");
+//in memory to store products
+let products = [];
+let currentId = 1; //initialized the starting ID
+
+
+const generateId = () => {
+    return currentId++; //return the current id and increment it
+}
+
+// --- Routes ---
+
+// 1. GET /products - Retrieve all products
+app.get('/products', (req, res) => {
+    try{
+        res.status(200).json({
+            success: true,
+            message: "Products retrieved successfully",
+            data: products
+        });
+    }catch(err) {
+        console.log(err);
+        res.status(500)
+            .json({
+                success: false,
+                message: "Internal server error"
+            })
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Product Manager!");
 })
 
 app.listen(PORT, () => {
